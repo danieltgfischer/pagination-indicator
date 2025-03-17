be782b0937ceb9d681c3f7afba7e37fc6744371e
Daniel Fischer <danieltgfischer@gmail.com>
2025-03-17 09:30:41 -0300

    chore: Initialize project with Expo, Tailwind CSS, and NativeWind setup.

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-white"
+      style={{ flex: 1, flexGrow: 1 }}
+    >
+      <Text>Hello World</Text>
+    </View>
+  );
+}
