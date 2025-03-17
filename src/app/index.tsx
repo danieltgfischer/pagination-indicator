fdeec0093182f1bb172a673f83fffe45c6217512
Daniel Fischer <danieltgfischer@gmail.com>
2025-03-17 10:15:57 -0300

    feat: Integrate Onboarding component into Home screen with state management

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Onboarding } from "@/components";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
-    <View
-      className="flex-1 items-center justify-center bg-white"
-      style={{ flex: 1, flexGrow: 1 }}
-    >
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Onboarding
+        selectedIndex={selectedIndex}
+        onIndexChange={(index) => setSelectedIndex(index)}
+        totalPages={5}
+      />
     </View>
   );
 }
